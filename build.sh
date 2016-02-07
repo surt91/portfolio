7ea8dca80c1d1d7bc4f2b57dfc9782acd9d73a84
Hendrik Schawe <surt91@gmail.com>
2016-02-07 12:13:47 +0100

    Logo eingefügt

--- build.sh
+++ build.sh
@@ -5,13 +5,13 @@ sphinx-intl update -p _build/locale -l de
 sphinx-intl build
 
 rm favicon.{png,ico}
-convert  -background none img/favicon.svg -resize 256x256 img/favicon.png
+convert  -background none img/logo.svg -resize 256x256 img/favicon.png
 convert img/favicon.png -define icon:auto-resize="256,128,96,64,48,32,16" img/favicon.ico
 
 rm -r deploy
 mkdir -p deploy
 
-cp index.html.* img/favicon.ico img/loading.gif deploy
+cp index.html.* img/favicon.ico img/logo.svg img/loading.gif deploy
 cp BingSiteAuth.xml googlee1eadb2ddedaa639.html sitemap.* deploy
 
 make -e SPHINXOPTS="-D language='de'" html
