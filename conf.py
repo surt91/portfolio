91fcc9ab86ce796c9484a9ef95d2a4866ff54952
Hendrik Schawe <surt91@gmail.com>
2017-08-02 19:03:01 +0200

    fix sidebar

--- conf.py
+++ conf.py
@@ -118,7 +118,10 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {"github_button": False}
+html_theme_options = {
+    "github_button": False,
+    "fixed_sidebar": True,
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
