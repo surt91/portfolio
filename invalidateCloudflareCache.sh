0221bc68ff7e6739d543609068aa3ecf2dcc223a
Hendrik Schawe <surt91@gmail.com>
2017-08-03 16:26:57 +0200

    do not show email

--- invalidateCloudflareCache.sh
+++ invalidateCloudflareCache.sh
@@ -3,5 +3,5 @@ echo "Clear Cloudflare's cache"
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/4f12df4345095997e5c542b9bd3914d4/purge_cache" \
      -H "Content-Type:application/json" \
      -H "X-Auth-Key:$CLOUDFLARE_TOKEN" \
-     -H "X-Auth-Email:surt91@gmail.com" \
+     -H "X-Auth-Email:$EMAIL" \
      --data '{"purge_everything":true}'
