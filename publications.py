6d51efe6ac4d1d667ddfb4fbb1cbd41019f7218c
Hendrik Schawe <surt91@gmail.com>
2019-07-28 22:18:34 +0200

    copyright comments and theme support for comments

--- publications.py
+++ publications.py
@@ -4,40 +4,48 @@ PUBLICATIONS = [
         "doi": "10.1209/0295-5075/113/30004",
         "arxiv": "1512.08554",
         "pdf": "2016_TSP_EPL.pdf",
+        "comment": "direct link is fine: point (2) in https://authors.epletters.net/documents/copyright.pdf 'all or part [..] including the authors' personal home page'",
     },
     {
         "doi": "10.1038/s41598-017-08531-8",
         "pdf": "2017_IFM_SciRep.pdf",
+        "comment": "direct link is fine: Open Access (creative commons license)",
     },
     {
         "doi": "10.1103/PhysRevE.96.062101",
         "arxiv": "1709.02638",
         "pdf": "2017_convex_highdim_PRE.pdf",
+        "comment": "direct link is fine: point 3. in https://journals.aps.org/authors/transfer-of-copyright-agreement 'The right to use all or part of the Article, including the APS-prepared version without revision or modification, on the author(s)’ web home page [..]'",
     },
     {
         "doi": "10.1103/PhysRevE.97.062159",
         "arxiv": "1804.02371",
         "pdf": "2018_convex_avoiding_PRE.pdf",
+        "comment": "direct link is fine: point 3. in https://journals.aps.org/authors/transfer-of-copyright-agreement 'The right to use all or part of the Article, including the APS-prepared version without revision or modification, on the author(s)’ web home page [..]'",
     },
     {
         "doi": "10.1209/0295-5075/124/40005",
         "arxiv": "1808.09246",
         "pdf": "2018_srn_EPL.pdf",
+        "comment": "direct link is fine: point (2) in https://authors.epletters.net/documents/copyright.pdf 'all or part [..] including the authors' personal home page'",
     },
     {
         "doi": "10.1103/PhysRevE.99.042104",
         "arxiv": "1901.05235",
         "pdf": "2019_lis_PRE.pdf",
+        "comment": "direct link is fine: point 3. in https://journals.aps.org/authors/transfer-of-copyright-agreement 'The right to use all or part of the Article, including the APS-prepared version without revision or modification, on the author(s)’ web home page [..]'",
     },
     {
         "doi": "10.1140/epjb/e2019-90667-y",
         "arxiv": "1811.04816",
         "pdf": "2019_bi_EPJB_accepted.pdf",
+        "comment": "direct link is forbidden: is explicitly not allowed 'He/she may not use the publisher’s version (the final article), which is posted on SpringerLink and other Springer websites, for the purpose of self-archiving or deposit.' instead I use the explicitly allowed accepted version 'Authors may self-archive the Author’s accepted manuscript of their articles on their own websites.' but we can share the view link https://rdcu.be/bumov",
     },
     {
         "doi": "10.1371/journal.pone.0215309",
         "arxiv": "1702.02821",
         "pdf": "2019_sat_PLOSONE.pdf",
+        "comment": "direct link is fine: Open Access (creative commons license)",
     },
 ]
 
