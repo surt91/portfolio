7286233e5e7b0561efbe843372e570464f3f23d7
Hendrik Schawe <surt91@gmail.com>
2019-09-04 16:55:43 +0200

    use iso codes for countries and list them in the timeline

--- events.py
+++ events.py
@@ -3,6 +3,7 @@ from datetime import date
 EVENTS = [
     {
         "date": date(2013, 9, 5),
+        "country": "DE",
         "place": "Carl von Ossietzky Universität Oldenburg",
         "name": "Bachelor of Science",
         "type": "graduation",
@@ -15,6 +16,7 @@ EVENTS = [
     {
         "date": date(2014, 8, 25),
         "date_end": date(2014, 9, 5),
+        "country": "DE",
         "place": "Oldenburg",
         "name": "5th International Summer School on Modern Compuational Science: Compuational Quantum Chemistry",
         "link": "https://www.mcs.uni-oldenburg.de/64155.html",
@@ -23,6 +25,7 @@ EVENTS = [
     {
         "date": date(2014, 3, 30),
         "date_end": date(2014, 4, 4),
+        "country": "DE",
         "place": "Dresden",
         "name": "DPG-Frühjahrstagung",
         "link": "https://dresden14.dpg-tagungen.de/",
@@ -36,6 +39,7 @@ EVENTS = [
     {
         "date": date(2015, 9, 20),
         "date_end": date(2015, 9, 25),
+        "country": "DE",
         "place": "Bad Honnef",
         "name": "Bad Honnef Physics School: Computational Physics of Complex and Disordered Systems",
         "link": "https://www.dpg-physik.de/veranstaltungen/2015/bad-honnef-physics-school-computational-physics-of-complex-and-disordered-systems",
@@ -48,6 +52,7 @@ EVENTS = [
     },
     {
         "date": date(2015, 11, 6),
+        "country": "DE",
         "place": "Carl von Ossietzky Universität Oldenburg",
         "name": "Master of Science",
         "type": "graduation",
@@ -60,6 +65,7 @@ EVENTS = [
     {
         "date": date(2016, 3, 1),
         "date_end": date(2019, 8, 31),
+        "country": "DE",
         "place": "Carl von Ossietzky Universität Oldenburg",
         "name": "Institut für Physik (IfP), AG Compphys",
         "title": "wissenschaftlicher Mitarbeiter",
@@ -68,6 +74,7 @@ EVENTS = [
     {
         "date": date(2016, 3, 6),
         "date_end": date(2016, 3, 11),
+        "country": "DE",
         "place": "Regensburg",
         "name": "DPG-Frühjahrstagung",
         "link": "https://regensburg16.dpg-tagungen.de/",
@@ -81,6 +88,7 @@ EVENTS = [
     {
         "date": date(2016, 9, 1),
         "date_end": date(2016, 9, 30),
+        "country": "FR",
         "place": "Université Paris-Sud, Orsay",
         "name": "Laboratoire de Physique Théorique et Modèles Statistiques (LPTMS)",
         "title": "research visitor",
@@ -89,6 +97,7 @@ EVENTS = [
     {
         "date": date(2016, 11, 24),
         "date_end": date(2016, 11, 26),
+        "country": "DE",
         "place": "Leipzig",
         "name": "CompPhys16, 17th International NTZ-Workshop on New Developments in Computational Physics",
         "link": "https://www.physik.uni-leipzig.de/~janke/CompPhys16/",
@@ -102,6 +111,7 @@ EVENTS = [
     {
         "date": date(2017, 3, 19),
         "date_end": date(2017, 3, 24),
+        "country": "DE",
         "place": "Dresden",
         "name": "DPG-Frühjahrstagung",
         "link": "https://dresden17.dpg-tagungen.de/",
@@ -122,7 +132,8 @@ EVENTS = [
     {
         "date": date(2017, 7, 16),
         "date_end": date(2017, 7, 29),
-        "place": "Bruneck, Italia",
+        "country": "IT",
+        "place": "Bruneck",
         "name": "Fundamental Problems in Statistical Physics XIV",
         "link": "https://fpspxiv.sciencesconf.org/",
         "type": "summerschool",
@@ -130,6 +141,7 @@ EVENTS = [
     {
         "date": date(2017, 11, 30),
         "date_end": date(2017, 12, 2),
+        "country": "DE",
         "place": "Leipzig",
         "name": "CompPhys17, 18th International NTZ-Workshop on New Developments in Computational Physics",
         "link": "https://www.physik.uni-leipzig.de/~janke/CompPhys17/",
@@ -143,6 +155,7 @@ EVENTS = [
     {
         "date": date(2018, 3, 11),
         "date_end": date(2018, 3, 16),
+        "country": "DE",
         "place": "Berlin",
         "name": "DPG-Frühjahrstagung",
         "link": "https://berlin18.dpg-tagungen.de/",
@@ -156,7 +169,8 @@ EVENTS = [
     {
         "date": date(2018, 7, 29),
         "date_end": date(2018, 8, 2),
-        "place": "Davis, USA",
+        "country": "US",
+        "place": "Davis, CA",
         "name": "XXX IUPAP Conference on Computational Physics",
         "link": "http://ccp2018.physics.ucdavis.edu",
         "type": "conference",
@@ -169,6 +183,7 @@ EVENTS = [
     {
         "date": date(2019, 3, 31),
         "date_end": date(2019, 4, 5),
+        "country": "DE",
         "place": "Regensburg",
         "name": "DPG-Frühjahrstagung",
         "link": "https://regensburg19.dpg-tagungen.de/",
@@ -181,6 +196,7 @@ EVENTS = [
     },
     {
         "date": date(2019, 3, 19),
+        "country": "DE",
         "place": "Carl von Ossietzky Universität Oldenburg",
         "name": "Doktor der Naturwissenschaften (Dr. rer. nat.)",
         "type": "graduation",
@@ -201,9 +217,15 @@ EVENTS = [
     {
         "date": date(2019, 9, 1),
         "date_end": date(2020, 8, 31),
-        "place": "Université de Cergy-Pontoise, France",
+        "country": "FR",
+        "place": "Université de Cergy-Pontoise",
         "name": "Laboratoire de Physique Théorique et Modélisation (LPTM)",
         "title": "Postdoc",
         "type": "work",
     },
 ]
+
+import pycountry
+for i in EVENTS:
+    if "country" in i:
+        i["country"] = pycountry.countries.get(alpha_2=i["country"]).name
